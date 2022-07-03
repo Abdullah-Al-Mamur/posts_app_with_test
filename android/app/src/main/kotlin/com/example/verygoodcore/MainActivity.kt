9849ce67bb6f9733e28992df330b9b3cc7ac9a42
Abdullah Al Mamur <almamur.official@gmail.com>
2022-07-03 11:26:14 +0600

    inital commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.posts_app_with_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,17 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:flutter/material.dart';
+import 'package:posts_app_with_test/app/app.dart';
+import 'package:posts_app_with_test/bootstrap.dart';
+import 'package:posts_app_with_test/di/service_locator.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await setUpServiceLocators();
+  await bootstrap(() => const App());
+}
